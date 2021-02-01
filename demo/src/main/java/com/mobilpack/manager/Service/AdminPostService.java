538df5b61118dea9ca0e598c456f055cc8734980
SeongHun_Cho <jng00153@gmail.com>
2021-02-01 16:31:36 +0900

    /postcreate api Controller 부분 작성 및 postModel 작성

---
+++ demo/src/main/java/com/mobilpack/manager/Service/AdminPostService.java
@@ -0,0 +1,14 @@
+package com.mobilpack.manager.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mobilpack.manager.Dao.Dao;
+
+@Service
+public class AdminPostService {
+	@Autowired
+	private Dao Dao;
+	
+	
+}
