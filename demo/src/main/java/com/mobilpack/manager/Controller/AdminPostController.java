dfaa1108deeb676ea8f26ae862c242c02be6c3a3
SeongHun_Cho <jng00153@gmail.com>
2021-02-03 11:50:34 +0900

    db에 파일 경로 저장시 \ 누락되는 부분 수정하여 경로 표현 문제 해결

--- demo/src/main/java/com/mobilpack/manager/Controller/AdminPostController.java
+++ demo/src/main/java/com/mobilpack/manager/Controller/AdminPostController.java
@@ -1,6 +1,9 @@
 package com.mobilpack.manager.Controller;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -41,36 +44,40 @@ public class AdminPostController {
 		String folderName = post.getPostindex();
 		System.out.println(post.getPostindex());
 		//기본 경로를 설정함
-		String basePath ="C:\\\\Users\\shjo2\\Desktop\\Hub"
-				+ "\\GitHub\\MobilPack\\demo\\src"
-				+ "\\main\\resources\\images";
+		String basePath =".\\\\upload";
 		//기본 경로에 postindex값을 넣어 게시글 별로 경로 생성
-		File folderPath = new File(basePath+"\\"+folderName);	
+		File folderPath = new File(basePath+"//"+folderName);	
 		// 해당 경로가 없을 겅우 자동으로 폴더 생성
 		if(!folderPath.exists()) {
 			folderPath.mkdir();
 		}
-		//파일을 받는다
+		//다중 파일 업로드를 위한 for문
     	for(MultipartFile file : files) {
+    		//파일 모델 객체 생성
     		FileModel RepeatModel = new FileModel();
+    		//파일명 변수에 저장
     		String originalName = file.getOriginalFilename();
+    		//파일 확장자 검사를 위해 split으로 확장자 구분
     		String[] nameCheck = 
     				originalName.split("\\.");
+    		//고유식별자 생성
     		UUID newName = UUID.randomUUID();
-    		System.out.println(newName+"."+nameCheck[1]);
-    		String filePath = folderPath + "\\" + newName+"."+nameCheck[1];
-    		System.out.println(filePath);
-    		File dest = new File(filePath);
+    		//고유식별자를 이용한 파일 생성
+    		String filePath = basePath+"\\\\"+folderName+"\\\\" + newName+"."+nameCheck[1];
+    		//png와 jpg 확장자만 받기 위해 예외처리 진행
     		if(nameCheck[1].equals("png")
     				   ||nameCheck[1].equals("jpg")) {
     					try {
-    			    		file.transferTo(dest);
-    			    		dest.length();
+    						//확장자가 맞을 경우 파일 업로드
+    						Path path = Paths.get(filePath);
+    					    Files.write(path, file.getBytes());
     					} 
     					catch(Exception e) {
+    						//확장자가 맞지 않을 경우 false를 반환하고 코드 중지
     						e.printStackTrace();
     						return "false";
     					}
+    					//파일모델에 이름,경로,고유식별명,게시글index를 넣고 insert함
     					RepeatModel.setPostindex(post.getPostindex());
     					RepeatModel.setFilename(originalName);
     					RepeatModel.setFilepath(filePath);
@@ -84,7 +91,7 @@ public class AdminPostController {
     					}
     				}
     		else {
-    			return "right extension";
+    			return "wrong extension";
     		}
     	}	
 	return "uploaded";
