0daeb848614d6c9fd19b3665b5c6bd1a940c036b
Lee Hwayong <dlghkdyd500@gmail.com>
2021-02-09 17:49:57 +0900

    JWT 서비스 편의를 위한 메서드 추가 (Description 확인해주세요)

--- demo/src/main/java/com/mobilpack/manager/Service/JwtService.java
+++ demo/src/main/java/com/mobilpack/manager/Service/JwtService.java
@@ -16,15 +16,28 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
+/**
+ * 토큰 발급 및 해석 서비스
+ * @author Leehwayong
+ *
+ */
 public class JwtService {
 	@Value("${jwt.salt}")
 	private String salt;
 
 	@Value("${jwt.expmin}")
+	/*
+	 * 만료시간 값을 Application properties에서 불러옵니다.
+	 */
 	private Long expireMin;
-	//expireTime value
 	
-	//관리자 jwt토큰 발급 method
+	
+	/**
+	 * 관리자 JWT토큰 발급 method
+	 * OverLoad된 메서드입니다.
+	 * @param AdminModel을 사용합니다.
+	 * @return admin_id와 name을 담은 JWT String 반환
+	 */
 	public String createJWT(final AdminModel admin)
 	{
 		final JwtBuilder builder = Jwts.builder();
@@ -39,7 +52,12 @@ public class JwtService {
 		final String jwt = builder.compact();
 		return jwt;
 	}
-	//사용자용 토큰발급 Overload//
+	/**
+	 * 유저 JWT토큰 발급 method
+	 * OverLoad된 메서드입니다.
+	 * @param UserModel을 사용합니다.
+	 * @return admin_id와 name을 담은 JWT String 반환
+	 */
 	public String createJWT (final UserModel user) {
 		final JwtBuilder builder = Jwts.builder();
 		builder.setHeaderParam("typ","JWT");
@@ -52,7 +70,13 @@ public class JwtService {
 		final String jwt = builder.compact();
 		return jwt;
 	}
-	//jwt Decryption method
+	/**
+	 * JWT Decryption Method
+	 * 토큰을 해석하여 Body를 반환합니다.
+	 * @param jwt String
+	 * @return claims의 body를 반환합니다.
+	 * @throws Exception JWT Key가 만료되었을때 오류가 납니다. 
+	 */
 	public Map<String, Object> getInfo(final String jwt) throws Exception
 	{
 		Jws<Claims> claims = null;
@@ -63,5 +87,14 @@ public class JwtService {
 		}
 		return claims.getBody();
 	}
-	
+	/**
+	 * 
+	 * @param jwt
+	 * @return
+	 * @throws Exception
+	 */
+	public String getAdminID(final String jwt) throws Exception {
+		String id = (String)this.getInfo(jwt).get("admin_id");
+		return id;
+	}
 }
